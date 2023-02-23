d7d7a1d8832f1d3f8bea37db20d9cb6b068dd02d
Maris Erts <mariserts84@gmail.com>
2023-02-23 09:55:41 +0200

    removed gunicorn

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="uvicorn_requests",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Maris Erts",
+    author_email="maris@plain.ie",
+
+    # Packages
+    packages=["uvicorn_requests"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="#",
+
+    #
+    license="LICENSE",
+    description="#",
+
+    # long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "Jinja2==3.1.2",
+        "uvicorn==0.20.0",
+    ],
+
+
+)
